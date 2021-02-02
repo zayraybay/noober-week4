7411f258cb32e5d99f618e75b9e47a06da33f1d4
zayraybay <76918035+zayraybay@users.noreply.github.com>
2021-02-02 12:51:07 -0600

    Initial commit

---
+++ noober.js
@@ -0,0 +1,13 @@
+async function pageLoaded() {
+  let response = await fetch('https://kiei451.com/api/rides.json')
+  let json = await response.json()
+
+  // writes the returned JSON to the console
+  console.dir(json)
+  
+  // 🔥 start here: write code to loop through the rides
+  
+}
+
+window.addEventListener('DOMContentLoaded', pageLoaded)
+
